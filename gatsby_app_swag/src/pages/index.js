ddc99724b53f31f2699b5c4480dd50e58f1770df
ManuelDevWeb <manueldeveloper17@gmail.com>
2021-10-22 03:23:04 -0500

    ADD Componente Jumbo

--- gatsby_app_swag/src/pages/index.js
+++ gatsby_app_swag/src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 
+// Importando componentes
+import { Jumbo } from "../components/index";
+
 import { Layout, SEO } from "../components"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Jumbo />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -13,4 +17,4 @@ const IndexPage = () => (
   </Layout>
 )
 
-export default IndexPage
+export default IndexPage;
